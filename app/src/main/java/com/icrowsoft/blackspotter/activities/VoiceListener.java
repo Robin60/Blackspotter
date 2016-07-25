352dce7f81dbeb002344b646e4b4d64a7e00e4a2
Jonathan Kibet <jonathankibet46@gmail.com>
2016-07-26 02:04:20 +0300

    Added new class and layout

---
+++ app/src/main/java/com/icrowsoft/blackspotter/activities/VoiceListener.java
@@ -0,0 +1,15 @@
+package com.icrowsoft.blackspotter.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by teardrops on 7/26/16.
+ */
+public class VoiceListener extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
