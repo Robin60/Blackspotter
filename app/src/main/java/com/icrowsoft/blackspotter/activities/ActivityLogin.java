8887e2ae13cc4c8cb032b5ec5b3e0e12f46d624a
Jonathan Kibet <jonathankibet46@gmail.com>
2016-10-11 12:22:59 +0300

    Added login xml activity

---
+++ app/src/main/java/com/icrowsoft/blackspotter/activities/ActivityLogin.java
@@ -0,0 +1,22 @@
+package com.icrowsoft.blackspotter.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.icrowsoft.blackspotter.R;
+
+/**
+ * Created by kibet-GDTL on 11/10/2016.
+ */
+
+public class ActivityLogin extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
